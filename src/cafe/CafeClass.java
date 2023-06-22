d9a6c18c90484be29760620abc9ddff53998546e
ciw <ciw@n.com>
2023-06-22 14:32:56 +0900

    5조 프로젝트

---
+++ src/cafe/CafeClass.java
@@ -0,0 +1,31 @@
+package cafe;
+
+import java.util.Scanner;
+
+public class CafeClass {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		
+		while(true) {
+			System.out.println("1. 메뉴선택");
+			System.out.println("2. 판매현황");
+			System.out.println("3. 정산하기");
+			System.out.print(">> ");
+			num = sc.nextInt();
+			
+			switch(num) {
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			}
+		}
+	}
+}
