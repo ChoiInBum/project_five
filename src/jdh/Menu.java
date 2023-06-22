0778f35d855954c41278b394d4d3c51ddcd63a24
dh <a@a.com>
2023-06-22 15:38:40 +0900

    메뉴추가

---
+++ src/jdh/Menu.java
@@ -0,0 +1,26 @@
+package jdh;
+
+
+import java.util.HashMap;
+
+public class Menu {
+	public void menu() {
+		HashMap<String, Integer> menu = new HashMap<>();
+		menu.put("아메리카노", 3000);
+		menu.put("카페라떼", 3500);
+		menu.put("카페모카", 4000);
+		menu.put("카라멜 마끼야또", 4500);
+		System.out.println("menu :"+menu);
+		
+		
+		
+		
+		
+	
+		 
+		
+	}
+	}
+ 
+
+
